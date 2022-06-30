97efa646c53777d9012a5e458e75614daff049cf
Kate <kath.tsv@yandex.ru>
2022-06-30 22:02:58 +0300

    Homework is ready

---
+++ Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите любое число");
+int number = Convert.ToInt32(Console.ReadLine());
+isolation (number);
+
+void isolation (int currentNumber)
+{
+ if (currentNumber / 100 > 0)
+ {
+    string crutch = Convert.ToString (currentNumber);
+    char result = crutch[2];
+    Console.WriteLine(result);
+ }
+ else 
+ {
+    Console.WriteLine("Третьей цифры нет.");
+ }
+}
---
+++ Task_15/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+Day (number);
+
+void Day (int currentNumber)
+{
+    if (currentNumber==6 || currentNumber ==7)
+    {
+        Console.WriteLine ("Это выходной! Чиллим!!!");
+    }
+    else if (currentNumber>0 && currentNumber <6)
+    {
+        Console.WriteLine ("Это будний день, работаем.");
+    }
+    else 
+    {
+        Console.WriteLine ("Такого дня недели ещё не придумали, попробуй снова.");
+    }
+}
